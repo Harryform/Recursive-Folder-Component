f21dbbaa10371436c172a31356a96bb25c7335a4
Harrison Bush <97026183+Harryform@users.noreply.github.com>
2022-06-22 10:56:48 -0400

    khaleesiBranch added lwcs for menu options

--- force-app/main/default/lwc/folderView/folderView.js
+++ force-app/main/default/lwc/folderView/folderView.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
 
-export default class FolderView extends LightningElement {}
+export default class FolderView extends LightningElement {
+  menuOption = 'folder';
+
+  get displayFolderMenu() {
+    return this.menuOption === 'folder';
+  }
+
+  get displayCreateMenu() {
+    return this.menuOption === 'create';
+  }
+
+  get displayImanageMenu() {
+    return this.menuOption === 'imanage';
+  }
+
+  handleMenuSelect(event) {
+    this.menuOption = event.detail.name;
+  }
+}
