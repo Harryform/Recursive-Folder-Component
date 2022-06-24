3b593d2c93eba2981f55f39064f8c55ae1744e78
Harrison Bush <97026183+Harryform@users.noreply.github.com>
2022-06-24 13:25:11 -0400

    halliesWorkBranch Hallie setup folder tree layout

--- force-app/main/default/lwc/folderViewMenu/folderViewMenu.js
+++ force-app/main/default/lwc/folderViewMenu/folderViewMenu.js
@@ -1,3 +1,114 @@
 import { LightningElement } from 'lwc';
 
-export default class FolderViewMenu extends LightningElement {}
+export default class FolderViewMenu extends LightningElement {
+  items = [
+  {
+    label: 'Correspondence',
+    name: '1',
+    expanded: false,
+    items: [
+      {
+        label: 'Correspondence with Clients',
+        name: '2',
+        expanded: false,
+        items: [
+          {
+            label: 'Emails',
+            name: '3',
+            expanded: false,
+            items: [],
+          },
+          {
+            label: 'Phone Calls',
+            name: '4',
+            expanded: false,
+            items: [],
+          },
+        ],
+      },
+      {
+        label: 'Correspondence with Insurer',
+        name: '2',
+        expanded: false,
+        items: [
+          {
+            label: 'Emails',
+            name: '3',
+            expanded: false,
+            items: [],
+          },
+          {
+            label: 'Phone Calls',
+            name: '4',
+            expanded: false,
+            items: [],
+          },
+        ]
+      },
+    ],
+  },
+  {
+    label: 'Expenses',
+    name: '1',
+    expanded: false,
+    items: [
+      {
+        label: 'Case Cost',
+        name: '2',
+        expanded: false,
+        items: [],
+      },
+      {
+          label: 'Client Cost',
+          name: '2',
+          expanded: false,
+          items: [],
+      },
+      {
+        label: 'Receipts',
+        name: '2',
+        expanded: false,
+        items: [],
+      },
+    ],
+  },
+  {
+    label: 'Incident Information',
+    name: '2',
+    expanded: false,
+    items: [
+      {
+        label: 'Damages',
+        name: '2',
+        expanded: false,
+        items: [
+          {
+            label: 'Description',
+            name: '3',
+            expanded: false,
+            items: [],
+          },
+          {
+            label: 'Photos',
+            name: '4',
+            expanded: false,
+            items: [],
+          },
+        ]
+      },
+      {
+        label: 'Injured Statement',
+        name: '2',
+        expanded: false,
+        items: [],
+      },
+      ]
+    },
+    {
+      label: 'Settlement',
+      name: '2',
+      expanded: false,
+      items: [],
+    },
+  ]
+}
