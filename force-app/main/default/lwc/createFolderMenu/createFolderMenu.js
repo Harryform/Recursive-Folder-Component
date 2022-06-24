156392728429eb79179e52d4761bac0c218aff8a
Harrison Bush <97026183+Harryform@users.noreply.github.com>
2022-06-24 13:00:13 -0400

    upNextBranch Added combobox to createFolderMenu

--- force-app/main/default/lwc/createFolderMenu/createFolderMenu.js
+++ force-app/main/default/lwc/createFolderMenu/createFolderMenu.js
@@ -5,12 +5,27 @@ export default class CreateFolderMenu extends LightningElement {
   myRecordId;
 
   get acceptedFormats() {
-      return ['.pdf', '.png'];
+    return ['.pdf', '.png'];
   }
 
   handleUploadFinished(event) {
-      // Get the list of uploaded files
-      const uploadedFiles = event.detail.files;
-      alert('No. of files uploaded : ' + uploadedFiles.length);
+    // Get the list of uploaded files
+    const uploadedFiles = event.detail.files;
+    alert('No. of files uploaded : ' + uploadedFiles.length);
+  }
+
+  value = 'inProgress';
+
+  get options() {
+    return [
+      { label: 'Correspondence', value: 'correspondence' },
+      { label: 'Expenses', value: 'expenses' },
+      { label: 'Incident Information', value: 'incident' },
+      { label: 'Settlement', value: 'settle' },
+    ];
+  }
+
+  handleChange(event) {
+    this.value = event.detail.value;
   }
 }
